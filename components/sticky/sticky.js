983bca6337e5a11fecc2a7ec92d645c3781558a4
1GR3 <ivo.gregurec@gmail.com>
2022-12-26 10:47:40 +0100

    sticky moved to components

--- components/sticky/sticky.js
+++ components/sticky/sticky.js
@@ -1,20 +1,22 @@
-
 sticky = document.getElementById("sticky_1");
 /*
 sticky_i_button = document.getElementsByClassName("i")[0];
 sticky_i_button.addEventListener("click", flipSticky);
 */
 
-document.getElementsByClassName("i")[0].addEventListener("click", flipSticky);
-document.getElementsByClassName("done")[0].addEventListener("click", flipSticky);
-/*
-sticky.onclick. = function() {alert("click");}
+// document.getElementsByClassName("i")[0].addEventListener("click", flipSticky);
+// document
+//   .getElementsByClassName("done")[0]
+//   .addEventListener("click", flipSticky);
 
+sticky.addEventListener("click", click);
+function click() {
+  alert("click");
+}
+/*
 sticky.getElementsByClassName("i").onclick = function() {alert("click");}
 */
 
-
-function flipSticky() {
-  sticky.classList.toggle("flipped");
-}
-
+// function flipSticky() {
+//   sticky.classList.toggle("flipped");
+// }
